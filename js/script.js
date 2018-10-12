90c17a4e2efe7e4310accc66091bd1136c5ad590
Safiy Cham <csafiy@yahoo.com>
2018-10-11 22:15:57 -0700

    Reset sound loop if a different key pressed after

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
+let lastKeyCode;
+
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-keycode="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-keycode="${e.keyCode}"]`);
 
     audio.currentTime = 0;
 
-    if (key.getAttribute("value") === "1") {
+    console.log("keycode: " + e.keyCode);
+    
+    if (key.getAttribute("value") === "1" && e.keyCode == lastKeyCode) {
         audio.setAttribute("src", "sounds/b.mp3");
         audio.play();
         key.classList.add('pressed');
         key.setAttribute("value", "2");
+        console.log(`lastKeyCode: ${lastKeyCode}`);
 
-    } else if (key.getAttribute("value") === "2") {
+    } else if (key.getAttribute("value") === "2" && e.keyCode == lastKeyCode) {
         audio.setAttribute("src", "sounds/c.mp3");
         audio.play();
         key.classList.add('pressed');
         key.removeAttribute("value");
+        console.log(`lastKeyCode: ${lastKeyCode}`);
     } else {
-        console.log("this key doesn't have a value yet.");
         audio.setAttribute("src", "sounds/a.mp3");
-        key.setAttribute("value", "1");
         audio.play();
+        console.log("this key doesn't have a value yet.");
+        lastKeyCode = e.keyCode;
+        console.log(`lastKeyCode: ${lastKeyCode}`);
+        key.setAttribute("value", "1");
         key.classList.add('pressed');
     }
 });
