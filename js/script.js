596df960aec409c9cb10f82ecf2371da223e253c
Safiy Cham <csafiy@yahoo.com>
2018-10-12 22:05:20 -0700

    Create object to hold keycode:letter to assist with file load process

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 let lastKeyCode;
+let letters = {65: "a", 66: "b", 67: "c"};
+// let letters = [{65: "a"}, {66: "b"}, {c: 67}];
+// console.log("letters: " + letters[1][66]);
+console.log("letters: " + letters[66]);
 
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-keycode="${e.keyCode}"]`);
@@ -9,20 +13,23 @@ window.addEventListener('keydown', function (e) {
     console.log("keycode: " + e.keyCode);
     
     if (key.getAttribute("value") === "1" && e.keyCode == lastKeyCode) {
-        audio.setAttribute("src", "sounds/b.mp3");
+        // file naming conventions: a1sound, a2sound, a3sound, b1img, b2img, b3img
+        audio.setAttribute("src", `sounds/${letters[e.keyCode]}2sound.mp3`);
         audio.play();
         key.classList.add('pressed');
         key.setAttribute("value", "2");
         console.log(`lastKeyCode: ${lastKeyCode}`);
 
     } else if (key.getAttribute("value") === "2" && e.keyCode == lastKeyCode) {
-        audio.setAttribute("src", "sounds/c.mp3");
+        audio.setAttribute("src", `sounds/${letters[e.keyCode]}3sound.mp3`);
         audio.play();
         key.classList.add('pressed');
         key.removeAttribute("value");
         console.log(`lastKeyCode: ${lastKeyCode}`);
     } else {
-        audio.setAttribute("src", "sounds/a.mp3");
+        // audio.setAttribute("src", "sounds/a.mp3");
+        console.log("letter: " + letters[e.keyCode]);
+        audio.setAttribute("src", `sounds/${letters[e.keyCode]}1sound.mp3`);
         audio.play();
         console.log("this key doesn't have a value yet.");
         lastKeyCode = e.keyCode;
