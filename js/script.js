8ca4422280914fca26eafe3dcb80c3aed16ec693
Safiy Cham <csafiy@yahoo.com>
2018-10-21 01:39:52 -0700

    JS to hide title if height less than 640px. Bulk of layout/styling done

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ window.addEventListener('keydown', function (e) {
     
     // 4 levels, ex: "A", "Apple", "Ant", "Airplane"
     if (key.getAttribute("value") == "1" && e.keyCode == lastKeyCode) {
+        
         // file naming conventions: a1sound, a2sound, a3sound, b1img, b2img, b3img
         audio.setAttribute("src", `sounds/${letters[e.keyCode]}2sound.mp3`);
         audio.play();
@@ -22,6 +23,7 @@ window.addEventListener('keydown', function (e) {
         console.log(`lastKeyCode: ${lastKeyCode}`);
 
     } else if (key.getAttribute("value") == "2" && e.keyCode == lastKeyCode) {
+        // img.classList.remove('rotate-img');
         audio.setAttribute("src", `sounds/${letters[e.keyCode]}3sound.mp3`);
         audio.play();
         img.setAttribute("src", `img/${letters[e.keyCode]}3.jpg`);
@@ -30,6 +32,7 @@ window.addEventListener('keydown', function (e) {
         key.removeAttribute("value");
         console.log(`lastKeyCode: ${lastKeyCode}`);
     } else if (key.getAttribute("value") == "3" && e.keyCode == lastKeyCode) {
+        // img.classList.remove('rotate-img');
         audio.setAttribute("src", `sounds/${letters[e.keyCode]}4sound.mp3`);
         audio.play();
         img.setAttribute("src", `img/${letters[e.keyCode]}4.jpg`);
@@ -38,6 +41,7 @@ window.addEventListener('keydown', function (e) {
         key.removeAttribute("value");
         console.log(`lastKeyCode: ${lastKeyCode}`);
     } else {
+        // img.classList.remove('rotate-img');
         console.log("letter: " + letters[e.keyCode]);
         audio.setAttribute("src", `sounds/${letters[e.keyCode]}1sound.mp3`);
         audio.play();
@@ -49,18 +53,40 @@ window.addEventListener('keydown', function (e) {
         key.setAttribute("value", "1");
         key.classList.add('pressed');
     }
+
+    // img.classList.remove('rotate-img');
 });
 
-window.addEventListener('keyup', function (e) {
-    const img = document.querySelector("img");
+// window.addEventListener('keyup', function (e) {
+//     const img = document.querySelector("img");
 
-    img.classList.remove('rotate-img');
-});
+//     img.classList.remove('rotate-img');
+// });
 
-function removeTransition(e) {
+function removeKeyPressed(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('pressed');
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('transitionend', removeKeyPressed));
+
+function removeImgRotate(e) {
+    if (e.propertyName !== "transform")
+        return;
+    this.classList.remove('rotate-img');
+}
+
+const img = document.querySelector('img');
+img.addEventListener('transitionend', removeImgRotate);
+
+// Hide title_sec if height less than 640px
+let test = screen.height;
+console.log("screen height: " + test);
+if (screen.height <= 640) {
+    const title = document.querySelector(".title_sec");
+    const window = document.querySelector(".window");
+    
+    title.style.display = "none";
+    window.style.marginTop = "20px";
+}
