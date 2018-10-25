e34b0b23b38434d837417bee6ec25fd526f689cc
Safiy Cham <csafiy@yahoo.com>
2018-10-25 00:26:36 -0700

    Add click event to each letter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const img = document.querySelector("img");
 const keys = document.querySelectorAll('.key');
+// 'letters' for file naming purpose to get correct letter for keycode
 const letters = {
     65: "a", 66: "b", 67: "c", 68: "d", 69: "e", 70: "f", 71: "g", 72: "h", 73: "i", 74: "j",
     75: "k", 76: "l", 77: "m", 78: "n", 79: "o", 80: "p", 81: "q", 82: "r", 83: "s", 84: "t",
@@ -7,14 +8,13 @@ const letters = {
 };
 let lastKeyCode;
 
-// Keydown loops thru 4 levels of key; ex: "a", "apple", "ant", "airplane"
-// Loop resets to lev1 when different key pressed
-window.addEventListener('keydown', function (e) {
+// Run this when key is pressed
+function abcLoop(e) {
     const audio = document.querySelector(`audio[data-keycode="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-keycode="${e.keyCode}"]`);
 
     // confirms value of key in loop
-    // file naming conventions: a1sound, a2sound, a3sound, b1, b2, b3
+    // file naming conventions: sound-> a1sound, a2sound, a3sound; img -> b1, b2, b3
     if (key.getAttribute("value") == "1" && e.keyCode == lastKeyCode) {
         audio.setAttribute("src", `sounds/${letters[e.keyCode]}2sound.mp3`);
         img.setAttribute("src", `img/${letters[e.keyCode]}2.jpg`);
@@ -42,7 +42,6 @@ window.addEventListener('keydown', function (e) {
         console.log(`lastKeyCode: ${lastKeyCode}`);
         console.log("this key is now assigned value: " + key.getAttribute("value"));
     }
-
     console.log("keycode: " + e.keyCode);
 
     audio.play();
@@ -50,15 +49,15 @@ window.addEventListener('keydown', function (e) {
     img.classList.add('rotate-img');
     key.classList.add('pressed');
     audio.currentTime = 0;
-});
+}
 
-// remove class 'pressed'
+// Remove class 'pressed'
 function removeKeyPressed(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('pressed');  // 'this' => keys
 }
 
-// remove class 'rotate-img'
+// Remove class 'rotate-img'
 function removeImgRotate(e) {
     if (e.propertyName !== "transform") return;
     this.classList.remove('rotate-img');  // 'this' => img
@@ -71,6 +70,49 @@ if (screen.height <= 640) {
 
     window.style.marginTop = "10px";
     title.style.display = "none";
+}
+
+// Keydown loops thru 4 levels of key; ex: "a", "apple", "ant", "airplane"
+// Loop resets to lev1 when different key is pressed
+window.addEventListener('keydown', function (e) {
+    abcLoop(e);
+});
+
+// code works, but needs serious refactor
+// Click on letters, instead of event keydown
+for (let i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('click', function () {
+        let ltr = i + 65;
+        const audio = document.querySelector(`audio[data-keycode="${ltr}"]`);
+        const key = document.querySelector(`.key[data-keycode="${ltr}"]`);
+
+        // confirms value of key in loop
+        // file naming conventions: sound-> a1sound, a2sound, a3sound; img -> b1, b2, b3
+        if (keys[i].getAttribute("value") == "1" && ltr == lastKeyCode) {
+            audio.setAttribute("src", `sounds/${letters[ltr]}2sound.mp3`);
+            img.setAttribute("src", `img/${letters[ltr]}2.jpg`);
+            keys[i].setAttribute("value", "2");
+        } else if (keys[i].getAttribute("value") == "2" && ltr == lastKeyCode) {
+            audio.setAttribute("src", `sounds/${letters[ltr]}3sound.mp3`);
+            img.setAttribute("src", `img/${letters[ltr]}3.jpg`);
+            keys[i].setAttribute("value", "3");
+        } else if (keys[i].getAttribute("value") == "3" && ltr == lastKeyCode) {
+            audio.setAttribute("src", `sounds/${letters[ltr]}4sound.mp3`);
+            img.setAttribute("src", `img/${letters[ltr]}4.jpg`);
+            keys[i].setAttribute("value", "4");
+        } else {
+            audio.setAttribute("src", `sounds/${letters[ltr]}1sound.mp3`);
+            img.setAttribute("src", `img/${letters[ltr]}1.jpg`);
+            lastKeyCode = ltr;
+            keys[i].setAttribute("value", "1");
+        }
+
+        audio.play();
+        img.style.top = "30%";
+        img.classList.add('rotate-img');
+        keys[i].classList.add('pressed');
+        audio.currentTime = 0;
+    });
 }
 
 // After transition animations end, run functions to remove classes
